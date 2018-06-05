2c8de5951a70cd61782680c318ed55f78f40b277
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-05 21:14:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,29 +1,23 @@
 class Application
 
-  @@items = ["Apples","Carrots","Pears"]
-
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
+    num_1 = Kernel.rand(1..5)
+    num_2 = Kernel.rand(1..5)
+    num_3 = Kernel.rand(1..5)
 
-      search_term = req.params["q"]
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
 
-      if @@items.include?(search_term)
-        resp.write "#{search_term} is one of our items"
-      else
-        resp.write "Couldn't find #{search_term}"
-      end
-
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
     else
-      resp.write "Path Not Found"
+      resp.write "You Lose"
     end
 
     resp.finish
   end
+
 end
